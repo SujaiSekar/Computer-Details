3567a10987a32be4d18c72ebce257e61f45e1d1a
SujaiSekar <Sujai1817sudee@gmail.com>
2022-03-13 18:59:54 +0530

    Computer details added successfully

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,32 @@
+package org.computer;
+
+public class Desktop extends Computer {
+
+	public void desktopSize() {
+		System.out.println("Desktop Size: 15 inch");
+	}
+	
+	public static void main(String[]args) {
+		
+		Desktop desk = new Desktop();
+		
+		desk.desktopSize();
+		desk.computerModel();
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
